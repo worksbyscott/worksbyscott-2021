578f91aa80ea5a60cb4402c1757800c61b386154
worksbyscott <info@worksbyscott.uk>
2021-01-22 20:38:27 +0000

    Added skrew scrolling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { AnimatePresence } from "framer-motion";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import useWindowSize from "./hooks/useWindowSize";
 
 //Styles
 import "./styles/App.scss";
@@ -40,39 +42,73 @@ const App = () => {
   const [initial, setInitial] = useState(false);
 
   useEffect(() => {
+    requestAnimationFrame(() => skewScrolling())
     setTimeout(() => {
       setIntroduction(true);
     }, INTRODUCTION_TIMEOUT);
   })
 
+  const size = useWindowSize();
+  const app = useRef();
+  const scrollContainer = useRef();
+
+  const data = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0
+  };
+
+  useEffect(() => {
+    setBodyHeight();
+  }, [size.height]);
+
+  const setBodyHeight = () => {
+    document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height
+      }px`;
+  };
+
+
+  const skewScrolling = () => {
+    data.current = window.scrollY;
+    data.previous += (data.current - data.previous) * data.ease;
+    data.rounded = Math.round(data.previous * 100) / 100;
+
+    const difference = data.current - data.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+    requestAnimationFrame(() => skewScrolling());
+  };
+
   return (
-    <>
-
-      <AnimatePresence exitBeforeEnter>
-        {introduction ? (
-          <div className="main">
-            <Route render={({ location }) => (
-              <AnimatePresence exitBeforeEnter>
-                <Switch location={location} key={location.pathname}>
-                  {routes.map(({ path, Component }) => (
-                    <Route
-                      exact
-                      path={path}
-                      key={path}
-                      render={() => <Component key={path + "Comp"} intro={introduction} />} />
-                  ))}
-                </Switch>
-              </AnimatePresence>
-            )} />
-          </div>
-        ) : (
-            <Introduction key="intro-component" />
-          )}
-      </AnimatePresence>
-
-
-
-    </>
+    <div ref={app} className="App">
+      <div ref={scrollContainer} className="scroll">
+        <AnimatePresence exitBeforeEnter>
+          {introduction ? (
+            <div className="main">
+              <Route render={({ location }) => (
+                <AnimatePresence exitBeforeEnter>
+                  <Switch location={location} key={location.pathname}>
+                    {routes.map(({ path, Component }) => (
+                      <Route
+                        exact
+                        path={path}
+                        key={path}
+                        render={() => <Component key={path + "Comp"} intro={introduction} />} />
+                    ))}
+                  </Switch>
+                </AnimatePresence>
+              )} />
+            </div>
+          ) : (
+              <Introduction key="intro-component" />
+            )}
+        </AnimatePresence>
+      </div>
+    </div>
   );
 }
 
